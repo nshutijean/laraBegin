80531a990949efe0d89125ce40ffb02b5502c375
2nchy <j.nshuti73@gmail.com>
2020-03-13 15:39:27 +0200

    globalize Vue filters for dates and text

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,21 +5,24 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
+//imports
 require('./bootstrap');
 
 window.Vue = require('vue');
+
 
 import { Form, HasError, AlertError } from 'vform'
 import VueRouter from 'vue-router';
 import Dashboard from './components/Dashboard.vue';
 import Profile from './components/Profile.vue';
 import Users from './components/Users.vue';
+import moment from 'moment';
 
 //Form is made global
 window.Form = Form;
 
-Vue.use(VueRouter)
-
+//use vue router
+Vue.use(VueRouter);
 
 //Routes
 let routes = [
@@ -39,6 +42,25 @@ const router = new VueRouter({
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component(HasError.name, HasError)
 Vue.component(AlertError.name, AlertError)
+
+//capitalize filter for first letter
+Vue.filter('capitalize', function(text) {
+    text = text.toString();
+    return text.charAt(0).toUpperCase() + text.slice(1);
+});
+
+//capitalize filter for all letters
+Vue.filter('upText', function(text) {
+    text = text.toString();
+    return text.toUpperCase();
+});
+
+//date formattin using moment.js
+Vue.filter('dateFormat', function(newFormat){
+    return moment(newFormat).format('MMMM Do YYYY');
+}) 
+
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
